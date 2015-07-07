bcaa63cfd53aa5d1210429d4dcb6bd168f10909e
Grey-Soldierman <gungamer101@hotmail.com>
2015-07-07 12:37:54 +0100

    Ammo box models & textures move to 16aa_crates_a3

--- addons/staticweapons/CfgMagazines.hpp
+++ addons/staticweapons/CfgMagazines.hpp
@@ -13,7 +13,7 @@ class CfgMagazines {
         author = "3LSR";
         displayName = ".50cal Ammo Box";
         descriptionshort = "100Rd .50cal. Used in L2A1 Heavy Machine Gun";
-        model = PATHTOF(data\ammobox_50cal.p3d);
+        model = "\16aa_crates_a3\ammobox_50cal.p3d";
         picture = PATHTOF(UI\w_ammobox_50cal_ca.paa);
         magazines[] = {};
         mass = 20;
@@ -24,7 +24,7 @@ class CfgMagazines {
         author = "3LSR";
         displayName = "7.62mm Ammo Box";
         descriptionshort = "200Rd 7.62mm Used in L7A2 General Purpose Machine Gun";
-        model = PATHTOF(data\ammobox_762.p3d);
+        model = "\16aa_crates_a3\ammobox_762.p3d";
         picture = PATHTOF(UI\w_ammobox_762_ca.paa);
         magazines[] = {};
         mass = 20;
@@ -35,7 +35,7 @@ class CfgMagazines {
         author = "3LSR";
         displayName = "40mm Ammo Box";
         descriptionshort = "96Rd 40mm. Used in L134A1 Grenade Machine Gun";
-        model = PATHTOF(data\ammobox_40mm.p3d);
+        model = "\16aa_crates_a3\ammobox_40mm.p3d";
         picture = PATHTOF(UI\w_ammobox_40mm_ca.paa);
         magazines[] = {};
         mass = 20;
