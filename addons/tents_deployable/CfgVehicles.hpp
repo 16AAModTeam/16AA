604e4f85a9a4e50d025fdb5a6cd44de591fbe28d
Grey-Soldierman <gungamer101@hotmail.com>
2015-08-18 18:35:02 +0100

    Added deployable Medical Tent

--- addons/tents_deployable/CfgVehicles.hpp
+++ addons/tents_deployable/CfgVehicles.hpp
@@ -22,6 +22,15 @@ class CfgVehicles
                     priority = 2;
                     picture = "\16aa_tents\icon\Large.paa";
                 };
+                class GVAR(place_tent_medical) {
+                    //displayName = CSTRING(Placedown);
+                    displayName = "Deploy Tent - Medical";
+                    condition = QUOTE([ARR_2(_player,'16aa_tent_medical_roll_item')] call ace_common_fnc_hasItem);
+                    statement = QUOTE([ARR_3(_player,'16aa_tent_medical_roll_item','16aa_tent_medical_small')] call lsr_staticweapons_fnc_placeTimer);
+                    showDisabled = 0;
+                    priority = 2;
+                    picture = "\16aa_tents\icon\Large.paa";
+                };
             };
         };
     };
@@ -39,6 +48,19 @@ class CfgVehicles
             };
         };
 	};
+    class 16aa_tent_medical_roll: Item_Base_F {
+        scope = 2;
+        scopeCurator = 2;
+        author = "3LSR";
+        displayName = "Tent Bag - Medical";
+        vehicleClass = "Items";
+        class TransportItems {
+            class 16aa_tent_medical_roll_item {
+                name = "16aa_tent_medical_roll_item";
+                count = 1;
+            };
+        };
+    };
     class ReammoBox_F;
     class 16aa_Basha_Large : ReammoBox_F {
         ace_dragging_canDrag = 1;
@@ -81,6 +103,31 @@ class CfgVehicles
                     distance = 5;
                     condition = "true";
                     statement = QUOTE([ARR_3(_target,_player,'16aa_basha_roll_item')] call lsr_staticweapons_fnc_pickupTimer);
+                    showDisabled = 0;
+                    exceptions[] = {};
+                    priority = 4;
+                };
+            };
+        };
+    };
+    class 16aa_tent_medical_small: ReammoBox_F
+    {
+        ace_dragging_canDrag = 0;
+        ace_dragging_dragPosition[] = {0,2.5,0};
+        ace_dragging_dragDirection = 0;
+        ace_dragging_canCarry = 0;
+        class ACE_Actions {
+            class ACE_MainActions {
+                selection = "interact";
+                distance = 5;
+                condition = "true";
+                displayName = "Interact";
+                class 16aa_Pickup_Tent_Medical {
+                    selection = "";
+                    displayName = "Disassemble Tent - Medical";
+                    distance = 5;
+                    condition = "true";
+                    statement = QUOTE([ARR_3(_target,_player,'16aa_tent_medical_roll_item')] call lsr_staticweapons_fnc_pickupTimer);
                     showDisabled = 0;
                     exceptions[] = {};
                     priority = 4;
--- addons/tents_deployable/CfgWeapons.hpp
+++ addons/tents_deployable/CfgWeapons.hpp
@@ -16,4 +16,18 @@ class CfgWeapons {
             mass = 20;
         };
     };
+    class 16aa_tent_medical_roll_item: ACE_ItemCore
+    {
+        scope = 2;
+        scopeCurator = 2;
+        author = "3LSR";
+        displayName = "Tent Bag - Medical";
+        descriptionshort = "Duffel bag used to deploy a medical tent.";
+        model = "\16aa_tents\Basha_packed.p3d";
+        picture = "\16aa_tents\icon\Packed.paa";
+        magazines[] = {};
+         class ItemInfo: InventoryItem_Base_F {
+            mass = 130;
+        };
+    };
 };
