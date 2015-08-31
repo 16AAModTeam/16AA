879ae5e99afbaa2c6c028b942d745f1ca782f904
Grey-Soldierman <gungamer101@hotmail.com>
2015-08-31 11:58:28 +0100

    Removed debug action

--- addons/jackal_scripts/CfgVehicles.hpp
+++ addons/jackal_scripts/CfgVehicles.hpp
@@ -14,7 +14,9 @@ class CfgVehicles{
 	};
 	class 16aa_Jackal2_BASE_D : Car_F {
 		class ACE_Actions: ACE_Actions{
-            class ACE_MainActions: ACE_MainActions{
+            class ACE_MainActions;
+            /*
+            ACE_MainActions{
                 class 16aa_Debug {
                 displayName = "Debug";
                 distance = 5;
@@ -23,8 +25,9 @@ class CfgVehicles{
                 showDisabled = 0;
                 exceptions[] = {};
                 priority = 5;
+                };
             };
-            };
+            */
             class 16aa_Dismount_GPMG {
                 //position = "[1,0,0.5]";
                 selection = "GPMG";
